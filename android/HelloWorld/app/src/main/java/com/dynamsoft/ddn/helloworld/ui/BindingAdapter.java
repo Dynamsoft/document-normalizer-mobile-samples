04e93b07953e777457b2fbf2e67300fb57a9f7a4
DynamsoftAllen <84954394+DynamsoftAllen@users.noreply.github.com>
2023-07-17 10:38:30 +0800

    fix： fix normalizedImage bug

--- android/HelloWorld/app/src/main/java/com/dynamsoft/ddn/helloworld/ui/BindingAdapter.java
+++ android/HelloWorld/app/src/main/java/com/dynamsoft/ddn/helloworld/ui/BindingAdapter.java
@@ -37,6 +37,8 @@ public class BindingAdapter {
             } catch (CaptureException e) {
                 e.printStackTrace();
             }
+        } else {
+            view.setImageBitmap(null);
         }
     }
 
